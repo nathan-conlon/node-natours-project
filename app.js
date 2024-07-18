59384e6abd4216b4d02748af413aaf4bc2fa6d2c
Nathan Conlon <3057234@ads.qub.ac.uk>
2024-07-18 17:05:47 +0100

    Environment variables added; MongoDB

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-const app = express();
+const app = express(`${__dirname}/public`);
 
 // 1) MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
